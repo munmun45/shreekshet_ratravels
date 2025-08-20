188180338ce60f7d79c536bef81d4c31d6e6e55f
munmun45 <manaranamunmun@gmail.com>
2025-08-20 11:49:20 +0530

    upbzjhvc

---
+++ config/footer.php
@@ -0,0 +1,96 @@
+<footer class="float-left w-100 position-relative main-box footer-con">
+        <img alt="vector" class="vector8 img-fluid position-absolute" src="assets/images/vector8.png">
+        <div class="container">
+            
+            <div class="middle-portion">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 col-sm-6 col-12 footer-logo-con">
+                        <a href="index.html">
+                            <figure class="footer-logo">
+                                <img src="assets/images/footer-logo.png" class="img-fluid" alt="">
+                            </figure>
+                        </a>
+                        <p class="text-size-16 footer-text">Lorem ipsum dolor sit amet, consectetur adipiscing elitsed do eiusmod
+                            tempororem ipsum dolor sit am econsect ametconsectetetur adipiscing.</p>
+                        <figure class="mb-0 payment-icon">
+                            <img src="assets/images/payment-card.png" class="img-fluid" alt="">
+                        </figure>
+                    </div>
+                    <div class="col-lg-1 col-md-1 col-sm-6 col-12 d-lg-block d-none">
+
+                    </div>
+                    <div class="col-lg-2 col-md-6 col-sm-6 col-12">
+                        <div class="links">
+                            <h4 class="heading">Quick Links</h4>
+                            <hr class="line">
+                            <ul class="list-unstyled mb-0">
+                                <li><a href="index.html" class="text-size-16 text text-decoration-none">Home</a></li>
+                                <li><a href="service.html" class="text-size-16 text text-decoration-none">Services</a></li>
+                                <li><a href="blog.html" class="text-size-16 text text-decoration-none">Latest News</a></li>
+                                <li><a href="booking.html" class="text-size-16 text text-decoration-none">Booking</a></li>
+                                <li><a href="destinations.html" class="text-size-16 text text-decoration-none mb-0">Destinations</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-6 col-sm-6 col-12">
+                        <div class="links var1">
+                            <h4 class="heading">Support</h4>
+                            <hr class="line">
+                            <ul class="list-unstyled mb-0">
+                                <li><a href="about.html" class="text-size-16 text text-decoration-none">About Us</a></li>
+                                <li><a href="contact.html" class="text-size-16 text text-decoration-none">Contact Us</a></li>
+                                <li><a href="team.html" class="text-size-16 text text-decoration-none">Team</a></li>
+                                <li><a href="faq.html" class="text-size-16 text text-decoration-none">FAQ</a></li>
+                                <li><a href="testimonial.html" class="text-size-16 text text-decoration-none mb-0">Testimonial</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-6 col-12 d-sm-block">
+                        <div class="icon">
+                            <h4 class="heading">Get in Touch</h4>
+                            <hr class="line">
+                            <ul class="list-unstyled mb-0">
+                                <li class="text-size-16 text"><span class="d-inline-block">Email:</span> <a href="mailto:info@shreekshetratravels.com" class="mb-0 text text-decoration-nonetext-size-16">info@shreekshetratravels.com</a></li>
+                                <li class="text-size-16 text"><span class="d-inline-block">Phone:</span> <a href="tel:+919938437982" class="mb-0 text text-decoration-nonetext-size-16">+919938437982</a></li>
+                                <li class="text-size-16 text1"><span class="d-inline-block">Phone:</span> <a href="tel:+919348341126" class="mb-0 text text-decoration-nonetext-size-16">+919348341126</a></li>
+                                <li class="social-icons">
+                                    <div class="circle"><a href="https://www.facebook.com/login/"><i class="fa-brands fa-square-facebook"></i></a></div>
+                                    <div class="circle"><a href="https://twitter.com/i/flow/login"><i class="fa-brands fa-square-x-twitter"></i></a></div>
+                                    <div class="circle"><a href="https://www.linkedin.com/login"><i class="fa-brands fa-linkedin"></i></a>
+                                    </div>
+                                    <div class="circle"><a href="https://www.pinterest.com/"><i class="fa-brands fa-square-pinterest"></i></a></div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="copyright-con">
+                <div class="row">
+                    <div class="col-12">
+                        <p class="text-size-16">Copyright ©2025 www.shreekshetratravels.com [All Rights Reserved]
+                            <p class="text-size-16">Design By <a target="_blank" href="https://www.somasindia.com/">Somas Technology India Pvt Ltd</a></p>
+                    </div>
+                </div>
+                <!-- copyright con -->
+            </div>
+            <!-- container -->
+        </div>
+        <!-- footer con -->
+    </section>
+
+    <!-- BACK TO TOP BUTTON -->
+    <button id="back-to-top-btn" title="Back to Top"></button>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/owl.carousel.js"></script>
+    <script src="assets/js/contact-form.js"></script>
+    <script src="assets/js/video-popup.js"></script>
+    <script src="assets/js/video-section.js"></script>
+    <script src="assets/js/jquery.validate.js"></script>
+    <script src="assets/js/wow.js"></script>
+    <script src="assets/js/counter.js"></script>
+    <script src="assets/js/custom.js"></script>
+    <script src="assets/js/search.js"></script>
---
+++ config/header.php
@@ -0,0 +1,85 @@
+<!-- TOP BAR SECTION -->
+<div class="w-100 float-left top-bar-con main-box">
+            <div class="container">
+                <div class="top-bar-inner-con d-flex align-items-center justify-content-between">
+                    <div class="left-con">
+                        <a href="mailto:info@shreekshetratravels.com"><i class="fa-solid fa-envelope"></i> info@shreekshetratravels.com</a>
+                        <!-- left con -->
+                    </div>
+                    <div class="right-con"><a href="tel:+919938437982"><i class="fa-solid fa-square-phone"></i> 9938437982</a>
+                        <!-- right con -->
+                    </div>
+                    <!-- top bar inner con -->
+                </div>
+                <!-- container -->
+            </div>
+            <!-- top bar con -->
+        </div>
+        <div class="clearfix"></div>
+        <!-- HEADER SECTION -->
+        <header class="w-100 flaot-left header-con main-box position-relative">
+            <div class="container">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <a class="navbar-brand" href="index.html">
+                        <figure class="mb-0">
+                            <img src="assets/images/logo-icon.png" alt="logo-icon">
+                        </figure>
+                    </a>
+                    <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        <span class="navbar-toggler-icon"></span>
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link p-0" href="index.html">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link p-0" href="about.html">About Us</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link p-0" href="destinations.html">Destination</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link p-0 dropdown-toggle" href="#" id="navbarDropdown3" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pages</a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown3">
+                                    <a class="dropdown-item" href="booking.html">Booking</a>
+                                    <a class="dropdown-item" href="faq.html">Faq</a>
+                                    <a class="dropdown-item" href="contact.html">Contact</a>
+                                    <a class="dropdown-item" href="team.html">Team</a>
+                                    <a class="dropdown-item" href="service.html">Service</a>
+                                    <a class="dropdown-item" href="testimonial.html">Testimonial</a>
+
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle p-0" href="blog.html" id="navbarDropdown4" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Blog</a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown4">
+                                    <a class="dropdown-item" href="blog.html">Blog</a>
+                                    <a class="dropdown-item" href="load-more.html">Load More</a>
+                                    <a class="dropdown-item" href="single-blog.html">Single Blog</a>
+                                    <a class="dropdown-item" href="one-column.html">One Column</a>
+                                    <a class="dropdown-item" href="two-column.html">Two Column</a>
+                                    <a class="dropdown-item" href="three-column.html">Three Column</a>
+                                    <a class="dropdown-item" href="three-colum-sidbar.html">Three Column Sidbar</a>
+                                    <a class="dropdown-item" href="four-column.html">Four Column</a>
+                                    <a class="dropdown-item" href="six-colum-full-wide.html">Six Column</a>
+                                </div>
+                            </li>
+                        </ul>
+                        <div class="header-contact">
+                            <ul class="list-unstyled mb-0">
+                                <li><a href="contact.html" class="live-chat-btn d-inline-block"><i class="fa-solid fa-comment-dots"></i>
+                                        Live Chat</a></li>
+                                <!-- list unstyled -->
+                            </ul>
+                            <!-- header contact -->
+                        </div>
+                        <!--  -->
+                    </div>
+                </nav>
+                <!-- container -->
+            </div>
+            <!-- header-con -->
+        </header>
