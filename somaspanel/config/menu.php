cd1ad2156e1048266c94d2209eddacf42fe2f618
munmun45 <manaranamunmun@gmail.com>
2025-08-20 19:21:43 +0530

    update

--- somaspanel/config/menu.php
+++ somaspanel/config/menu.php
@@ -2,53 +2,43 @@
 
     <ul class="sidebar-nav" id="sidebar-nav">
 
-    <li class="nav-item">
-    <a class="nav-link collapsed" href="index">
-        <i class="bi bi-speedometer2"></i>
-        <span>Dashboard</span>
-    </a>
-</li>
-
-<li class="nav-item">
-    <a class="nav-link collapsed" href="destination">
-        <i class="bi bi-geo-alt-fill"></i> <!-- location icon -->
-        <span>Destination Manager</span>
-    </a>
-</li>
-
-<li class="nav-item">
-    <a class="nav-link collapsed" href="activities">
-        <i class="bi bi-bicycle"></i> <!-- activity/sports icon -->
-        <span>Activities Manager</span>
-    </a>
-</li>
-
-<li class="nav-item">
-    <a class="nav-link collapsed" href="blog">
-        <i class="bi bi-journal-text"></i> <!-- blog/article icon -->
-        <span>Blog Manager</span>
-    </a>
-</li>
-
-<li class="nav-item">
-    <a class="nav-link collapsed" href="booking">
-        <i class="bi bi-ticket-perforated-fill"></i> <!-- booking/ticket icon -->
-        <span>Activity & Destination Bookings</span>
-    </a>
-</li>
-
-<li class="nav-item">
-    <a class="nav-link collapsed" href="booking-hotel">
-        <i class="bi bi-building"></i> <!-- hotel/building icon -->
-        <span>Hotel Booking Manager</span>
-    </a>
-</li>
-
-
-      
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="index">
+                <i class="bi bi-speedometer2"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
 
 
 
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="destination">
+                <i class="bi bi-geo-alt-fill"></i> <!-- location icon -->
+                <span>Destination Manager</span>
+            </a>
+        </li>
+
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="blog">
+                <i class="bi bi-journal-text"></i> <!-- blog/article icon -->
+                <span>Blog Manager</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="booking">
+                <i class="bi bi-ticket-perforated-fill"></i> <!-- booking/ticket icon -->
+                <span>Activity & Destination Bookings</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="booking-hotel">
+                <i class="bi bi-building"></i> <!-- hotel/building icon -->
+                <span>Hotel Booking Manager</span>
+            </a>
+        </li>
 
 
     </ul>
